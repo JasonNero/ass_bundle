52ba8ec4aa4e3928d222a9b3bb365431c244f09b
Jason Schuehlein <jason@schuehlein.eu>
2022-11-30 13:13:05 +0100

    add remap_only

--- ass_bundle/cli.py
+++ ass_bundle/cli.py
@@ -15,11 +15,13 @@ app = typer.Typer(
 def run(
     source: Path = typer.Argument(..., help="Source directory of the ass files."),
     target: Path = typer.Argument(..., help="Target directory for all resources."),
+    remap_only: bool = typer.Option(False, "--remap-only", help="Skips the copy process, only remaps the paths."),
     dry_run: bool = typer.Option(False, "--dry-run", help="Don't write any files."),
 ):
     """Run via commandline."""
     file_map = core.remap_ass_files(source, target, fetch_only=dry_run)
-    core.copy_images(file_map, target, dry_run=dry_run)
+    if not remap_only:
+        core.copy_images(file_map, target, dry_run=dry_run)
 
 
 @app.command()
