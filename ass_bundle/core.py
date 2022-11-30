d260bc9b5d4ee9593c489b9241b0f2fb02f4a1c5
Jason Schuehlein <jason@schuehlein.eu>
2022-11-30 14:33:50 +0100

    make sure ai.dll is found

--- ass_bundle/core.py
+++ ass_bundle/core.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import sys
 from contextlib import contextmanager
@@ -14,6 +15,7 @@ if sys.platform == "darwin":
     sys.path.append(f"/Applications/Autodesk/Arnold/mtoa/{version}/scripts")
 elif sys.platform == "win32":
     sys.path.append(f"C:/Program Files/Autodesk/Arnold/maya{version}/scripts")
+    os.environ += f"C:/Program Files/Autodesk/Arnold/maya{version}/bin"  # Make sure the `ai.dll` file is found.
 else:
     raise OSError("Unknown or unsupported OS!")
 
